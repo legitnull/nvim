cc5f05ba39a60af2f342b965d5b8754f6a0122e6
Yupu Feng <ypfeng@baai.ac.cn>
2025-12-29 11:33:22 +0800

    increase git timeout to 600s

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,6 +15,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  git = {
+    timeout = 600, -- seconds (e.g. 10 minutes)
+  },
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
