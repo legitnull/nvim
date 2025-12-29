bdb13b67e6e9a8efb95aee5de19e6a7bf6618d9c
Yupu Feng <ypfeng@baai.ac.cn>
2025-12-29 11:36:11 +0800

    Update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.g.autoformat = false
+vim.g.autoformat = true
+--- Disable UI animations
+vim.g.snacks_animate = false
