fca8e17d9321e0e052717a673d56cd7a1583bd71
Yupu Feng <feng.yu.pu0330@gmail.com>
2026-02-12 14:42:49 +0800

    disable autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 --
-vim.g.autoformat = true
+vim.g.autoformat = false
 --- Disable UI animations
 vim.g.snacks_animate = false
 vim.opt.clipboard = "unnamedplus"
-vim.opt.mousescroll = "ver:2,hor:2"
+vim.opt.mousescroll = "ver:5,hor:5"
+vim.opt.exrc = true
